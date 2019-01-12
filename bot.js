303de0d08740848ed8172a88bc91a1653204e0f8
Rirexe <45144673+Rirexe@users.noreply.github.com>
2019-01-12 19:07:24 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("533238541644333057")
+setInterval(function() {
+channel.send(`هنا كلام ال سبام`);
+}, 30)
+})
+
+client.login("NTMzMjI5MDIxMjQxMDE2MzIw.Dxuh8Q.hFhhk6hu5tUNkrBgU4P4EUe9N0s");
